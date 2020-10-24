3c2306768d1815716f4ead0a056a0d6c418f63fa
miloszg <milosz.gus@gmail.com>
2020-10-24 12:00:46 +0200

    lab 3 mam done

--- app/src/main/java/pl/milosz/mam_1/MainActivity.java
+++ app/src/main/java/pl/milosz/mam_1/MainActivity.java
@@ -2,6 +2,8 @@ package pl.milosz.mam_1;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.GpsSatellite;
+import android.location.GpsStatus;
 import android.location.Location;
 
 import androidx.annotation.NonNull;
@@ -13,6 +15,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Looper;
 import android.util.Log;
 import android.widget.TextView;
@@ -23,21 +26,22 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
-    // dupa
     private LocationManager lm;
-
+    private static final String FILENAME = "MAM3.txt";
     // Pozycja
     Location currentLocation;
     Location previousLocation;
 
-    Location GGLocation;
-    Location WETILocation;
-    Location OBCLocation;
-    ArrayList<Location> locations = new ArrayList<>();
-    TextView speedText, directionText, currentLocationText, previousLocationText;
+    TextView speedText, directionText, currentLocationText, previousLocationText, statusText;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         }
 
         currentLocation = new Location("Provider");
-
+        statusText = findViewById(R.id.statusText);
         speedText = findViewById(R.id.speedText);
         directionText = findViewById(R.id.directionText);
         currentLocationText = findViewById(R.id.currentLocationText);
@@ -62,35 +66,71 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     }
 
     public void getLocation() {
+
         lm = (LocationManager) getSystemService(LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 0, this);
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 3, this);
+            lm.addGpsStatusListener(new GpsStatus.Listener() {
+                @Override
+                public void onGpsStatusChanged(int event) {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        GpsStatus gps = lm.getGpsStatus(null);
+                        String status = "";
+                        for(GpsSatellite sat:gps.getSatellites()){
+                            status+= "PRN:"+sat.getPrn()+"\tSNR:" + sat.getSnr()+"\n";
+                        }
+                        statusText.setText(status);
+                    }
+                }
+            });
         }
+    }
 
+    private void writeToFile(String filename,String content){
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FILENAME, MODE_PRIVATE);
+            fos.write(content.getBytes());
+            Toast.makeText(this, "Zapisano do: "+getFilesDir() +"/"+FILENAME, Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fos!=null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(4);
         currentLocation.setLatitude(location.getLatitude());
         currentLocation.setLongitude(location.getLongitude());
         currentLocation.setTime(location.getTime());
-        currentLocationText.setText("lat: "+currentLocation.getLatitude() +" lon: "+currentLocation.getLongitude());
+        currentLocationText.setText("lat: "+df.format(currentLocation.getLatitude()) +" lon: "+df.format(currentLocation.getLongitude()));
         if(previousLocation!=null){
             float bearing = previousLocation.bearingTo(currentLocation);
-            float distance = previousLocation.distanceTo(currentLocation);
-            float time = currentLocation.getTime() - previousLocation.getTime();
-            Log.i("DUPA", String.valueOf(distance));
-            Log.i("DUPA", String.valueOf(currentLocation.getTime()));
-            Log.i("DUPA", String.valueOf(previousLocation.getTime()));
-            Log.i("DUPA", String.valueOf(time));
-
+            float distance = previousLocation.distanceTo(currentLocation); // jednostka metry
+            float time = currentLocation.getTime() - previousLocation.getTime(); // jednostka milisekundy
             float speed = (float) (distance*1000 / (currentLocation.getTime() - previousLocation.getTime()));
-
-            speedText.setText(String.valueOf(speed)+" m/s");
-            directionText.setText(String.valueOf(bearing));
-            previousLocationText.setText("lat: "+previousLocation.getLatitude() +" lon: "+previousLocation.getLongitude());
-
+            speedText.setText(df.format((speed))+" m/s");
+            directionText.setText(df.format((bearing)));
+            previousLocationText.setText("lat: "+df.format(previousLocation.getLatitude()) +" lon: "+df.format(previousLocation.getLongitude()));
+            String content="";
+            content += "Status: "+statusText.getText()+"\n";
+            content += "Pozycja aktulana: "+currentLocationText.getText()+"\n";
+            content += "Pozycja poprzednia: "+previousLocationText.getText()+"\n";
+            content += "Prędkość poruszania: "+speedText.getText()+"\n";
+            content += "Kierunek poruszania: "+directionText.getText()+"\n";
+            writeToFile("MAM3",content);
         }
         if(currentLocation!=null){
             previousLocation = new Location("Provider");
@@ -98,10 +138,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             previousLocation.setLatitude(currentLocation.getLatitude());
             previousLocation.setTime(currentLocation.getTime());
         }
-
-
     }
 
+    // inna opcja na pobieranie lokacji
 //    private void GetLocation() {
 //        final LocationRequest locationRequest = new LocationRequest();
 //        locationRequest.setInterval(3000);
